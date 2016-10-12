66b2e26795454d3832145259e0768ce7fbe7faca
John Yin <10142130151_ecnu@outlook.com>
2016-10-12 18:32:15 +0800

    Add command structure

--- main.cpp
+++ main.cpp
@@ -4,16 +4,27 @@ Email: 10142130151_ecnu@outlook.com
 
 */
 
-#include <unistd.h>
-#include <fcntl.h>
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <string.h>
+#include <unistd.h>
 
 #define TRUE (-1)
 #define FALSE (0)
-#define MAX_STRING 200
-#define MAX_ARGS 500
+
+#define MAX_ARGS (50) //the max count of args
+#define MAX_CMD (1000)
+#define MAX_GRP (100)
+#define BUFF (4096)
+
+
+
 
 char prompt[MAX_STRING] = "mysh >";
 
